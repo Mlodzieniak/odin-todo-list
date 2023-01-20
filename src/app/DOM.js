733e1721f2f4e9dacc0712c54e1232cded33c7e7
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-01-20 10:08:49 +0100

    made printProjects module for DOM manipulation

--- src/app/DOM.js
+++ src/app/DOM.js
@@ -1,28 +1,10 @@
 import projectsUI from "./projectsUI";
+import printProjects from "./domMethods/printProjects";
 
-const list = document.querySelector(".projects-list");
 const newProjectBTN = document.querySelector(".new-project-btn");
 
 const dom = () => ({
-  printProjects(projects) {
-    list.innerHTML = "";
-    projects.forEach((project) => {
-      const label = document.createElement("div");
-      const name = document.createElement("div");
-      const creationDate = document.createElement("div");
-      const todos = document.createElement("div");
-
-      name.textContent = `Name: ${project.getProjectName()}`;
-      creationDate.textContent = `Creation date: ${project.getCreationDate()}`;
-      todos.textContent = `Tasks to perform: ${project.todoUI.tasksToComplete}`;
-      label.appendChild(name);
-      label.appendChild(creationDate);
-      label.appendChild(todos);
-
-      label.classList.add("project-label");
-      list.appendChild(label);
-    });
-  },
+  printProjects: (projects) => printProjects(projects),
   newProject() {
     projectsUI.add();
     this.printProjects(projectsUI.getProjects);
---
+++ src/app/domMethods/printProjects.js
@@ -0,0 +1,35 @@
+const list = document.querySelector(".projects-list");
+const printProjects = (projects) => {
+  list.innerHTML = "";
+  projects.forEach((project) => {
+    const label = document.createElement("div");
+    const labelData = document.createElement("div");
+    const labelButtons = document.createElement("div");
+
+    // Project data
+    const name = document.createElement("div");
+    const creationDate = document.createElement("div");
+    const todos = document.createElement("div");
+
+    // Buttons
+    const removeBTN = document.createElement("button");
+    removeBTN.setAttribute("type", "button");
+    removeBTN.classList.add("remove-btn");
+    removeBTN.innerHTML = "\u274C";
+
+    name.textContent = `Name: ${project.getProjectName()}`;
+    creationDate.textContent = `Creation date: ${project.getCreationDate()}`;
+    todos.textContent = `Tasks to perform: ${project.todoUI.tasksToComplete}`;
+    labelData.appendChild(name);
+    labelData.appendChild(creationDate);
+    labelData.appendChild(todos);
+
+    labelButtons.appendChild(removeBTN);
+
+    label.classList.add("project-label");
+    label.appendChild(labelData);
+    label.appendChild(labelButtons);
+    list.appendChild(label);
+  });
+};
+export default printProjects;
