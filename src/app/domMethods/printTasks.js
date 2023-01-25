c938b9785a11684e31636d8e7fb5ed315f5e0db4
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-01-25 16:24:55 +0100

    hide task info button added

--- src/app/domMethods/printTasks.js
+++ src/app/domMethods/printTasks.js
@@ -2,48 +2,59 @@ import tasksUI from "../tasksUI";
 
 const printTasks = (project) => {
   const container = document.querySelector(".project-content");
-  const todo = document.createElement("div");
+  // const todo = document.createElement("div");
+  const todo = document.querySelector(".todo-list");
   container.appendChild(todo);
   const tasks = project.tasksUI.getList();
-  console.log("XDD");
-  console.log(tasks);
   function print(task) {
     const label = document.createElement("div");
     const taskName = document.createElement("div");
     const taskCreationDate = document.createElement("div");
     const taskDeadline = document.createElement("input");
     const taskDescription = document.createElement("textarea");
+    const buttons = document.createElement("div");
     const viewInfoBTN = document.createElement("button");
+    const hideInfoBTN = document.createElement("button");
     const removeBTN = document.createElement("button");
     const progressBTN = document.createElement("button");
 
     label.classList.add("task-label");
     taskName.classList.add("task-name");
     viewInfoBTN.classList.add("label-buttons");
+    hideInfoBTN.classList.add("label-buttons");
     removeBTN.classList.add("label-buttons");
-    taskDeadline.setAttribute("input", "date");
-    taskDescription.setAttribute("rows", "5");
-    taskDescription.setAttribute("cols", "50");
+    label.setAttribute("data-id", task.getID());
+    taskDeadline.setAttribute("type", "date");
+    // taskDescription.setAttribute("rows", "5");
+    // taskDescription.setAttribute("cols", "50");
+    taskDescription.setAttribute("placeholder", "Comment");
 
-    taskName.textContent = `Name: ${task.getTaskName()}`;
+    taskName.textContent = `Name: ${task.getTitle()}`;
     taskCreationDate.textContent = `Creation date: ${task.getCreationDate()}`;
     taskDeadline.textContent = `Deadline: ${task.getDeadlineDate()}`;
-    taskDescription.innerHTML =
-      // label.setAttribute("data-id", task.getID());
-      todo.appendChild(label);
+    // taskDescription.innerHTML =
+    // label.setAttribute("data-id", task.getID());
+    todo.appendChild(label);
 
     removeBTN.innerHTML = "\u274C";
     viewInfoBTN.innerHTML = "More Info";
-
-    label.append(taskName, viewInfoBTN, removeBTN, progressBTN);
+    hideInfoBTN.innerHTML = "Hide Info";
+    progressBTN.innerHTML = ">>";
+    buttons.append(viewInfoBTN, removeBTN, progressBTN);
+    label.append(taskName, buttons);
     removeBTN.addEventListener("click", () => {
       tasksUI.removeByID(task.getID());
       todo.removeChild(label);
     });
     viewInfoBTN.addEventListener("click", () => {
-      label.insertBefore(taskCreationDate, viewInfoBTN);
-      label.insertBefore(taskDeadline, viewInfoBTN);
-      label.insertBefore(taskDescription, viewInfoBTN);
+      label.append(taskCreationDate, taskDeadline, taskDescription);
+      buttons.replaceChild(hideInfoBTN, viewInfoBTN);
+    });
+    hideInfoBTN.addEventListener("click", () => {
+      label.removeChild(taskCreationDate);
+      label.removeChild(taskDeadline);
+      label.removeChild(taskDescription);
+      buttons.replaceChild(viewInfoBTN, hideInfoBTN);
     });
   }
 
@@ -59,7 +70,6 @@ const printTasks = (project) => {
         print(task);
       }
     } else if (taskLabels.length === 0) {
-      console.log("XD");
       print(task);
     }
   });
--- src/app/taskDOM.js
+++ src/app/taskDOM.js
@@ -5,39 +5,53 @@ import printTasks from "./domMethods/printTasks";
 
 const taskDom = (project) => {
   const tasksWrapper = document.querySelector(".project-content");
+  const todoTasks = document.createElement("div");
+  const doingTasks = document.createElement("div");
+  const doneTasks = document.createElement("div");
+
   const newTaskBTN = document.createElement("button");
   const newTaskName = document.createElement("input");
   const acceptBTN = document.createElement("button");
   const nameError = document.createElement("div");
 
+  todoTasks.classList.add("task-list-wrapper");
+  todoTasks.classList.add("todo-list");
+
+  tasksWrapper.innerHTML = "";
+
   newTaskName.setAttribute("type", "text");
   newTaskName.setAttribute("placeholder", "task's name");
+  newTaskName.value = "TASK";
 
   newTaskBTN.textContent = "+";
   acceptBTN.textContent = "Accept";
   nameError.textContent = "Name should have between 3 and 30 characters.";
 
+  tasksWrapper.appendChild(todoTasks);
+  tasksWrapper.appendChild(doingTasks);
+  tasksWrapper.appendChild(doneTasks);
   newTaskBTN.addEventListener("click", () => {
-    tasksWrapper.appendChild(newTaskName);
-    tasksWrapper.appendChild(acceptBTN);
-    tasksWrapper.removeChild(newTaskBTN);
+    todoTasks.appendChild(newTaskName);
+    todoTasks.appendChild(acceptBTN);
+    todoTasks.removeChild(newTaskBTN);
   });
   acceptBTN.addEventListener("click", () => {
     if (newTaskName.value.length < 3 || newTaskName.value.length > 30) {
-      tasksWrapper.insertBefore(nameError, newTaskName);
+      todoTasks.insertBefore(nameError, newTaskName);
     } else {
-      tasksUI().add(newTaskName.value);
-      console.log(tasksUI().getList);
+      project.tasksUI.add(newTaskName.value);
+      // console.log(tasksUI().getList());
       printTasks(project);
-      tasksWrapper.removeChild(newTaskName);
-      tasksWrapper.removeChild(acceptBTN);
-      tasksWrapper.appendChild(newTaskBTN);
-      if (nameError.parentNode === tasksWrapper) {
-        tasksWrapper.removeChild(nameError);
+      todoTasks.removeChild(newTaskName);
+      todoTasks.removeChild(acceptBTN);
+      todoTasks.appendChild(newTaskBTN);
+      if (nameError.parentNode === todoTasks) {
+        todoTasks.removeChild(nameError);
       }
     }
   });
-  tasksWrapper.appendChild(newTaskBTN);
+  printTasks(project);
+  todoTasks.appendChild(newTaskBTN);
 
   // // DUMMY's
   // const event = new Event("click");
